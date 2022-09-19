c6862a16795b67d7dac34bfdc55661ca4dbd6ef2
Afo <obaone01@gmail.com>
2022-09-19 02:27:23 -0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
