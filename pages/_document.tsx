7d8f4eb003a7ae967113f05274e6977da6ba17a5
Afo <obaone01@gmail.com>
2022-10-07 14:22:13 -0700

    added logos and meta link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import { DEFAULT_SEO } from 'lib/config/seo'
 import { Head, Html, Main, NextScript } from 'next/document'
 
 const Document = () => {
@@ -5,10 +6,14 @@ const Document = () => {
     <Html>
       <Head>
         <meta name="title" content="WEFA" />
-        <meta
-          name="description"
-          content="Connecting Our Environment to the Digital World."
-        />
+        <meta name="description" content={DEFAULT_SEO.description} />
+        <meta property="og:url" content="https://wefa.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={DEFAULT_SEO.title} />
+        <meta property="og:description" content={DEFAULT_SEO.description} />
+        <meta property="og:image" content="/logo/color.png" />
+        <meta property="og:image:width" content="150" />
+        <meta property="og:image:height" content="60" />
         <link rel="icon" href="/favicon.png" />
         <link rel="shortcut icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
