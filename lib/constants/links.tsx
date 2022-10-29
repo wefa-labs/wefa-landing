346d5cdc696b37119c5e1a1728e65742e5f7e100
Afo <obaone01@gmail.com>
2022-10-29 00:08:22 -0700

    updated social links

--- lib/constants/links.tsx
+++ lib/constants/links.tsx
@@ -1,14 +1,26 @@
 import { LinkType } from 'components/Link'
 
 import Twitter from '../../public/twitter.svg'
-import Instagram from '../../public/instagram.svg'
+// import Instagram from '../../public/instagram.svg'
+import LinkedIn from '../../public/linkedin.svg'
+import Github from '../../public/github.svg'
 
 export const socialLinks: LinkType[] = [
   { link: 'https://twitter.com/_wefa_', title: 'Twitter', Icon: <Twitter /> },
+  // {
+  //   link: 'https://instagram.com/wefa_app',
+  //   title: 'Instagram',
+  //   Icon: <Instagram />,
+  // },
   {
-    link: 'https://instagram.com/wefa_app',
-    title: 'Instagram',
-    Icon: <Instagram />,
+    link: 'https://www.linkedin.com/company/wefa-app',
+    title: 'LinkedIn',
+    Icon: <LinkedIn />,
+  },
+  {
+    link: 'https://github.com/wefa-tech',
+    title: 'Github',
+    Icon: <Github />,
   },
 ]
 export const companyLinks: LinkType[] = [
