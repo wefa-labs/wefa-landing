3d62263313cf71f3a5f366e87987d9a51a43a1c6
Afo <obaone01@gmail.com>
2023-02-27 19:29:08 -0800

    added cross orgin link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,7 +21,7 @@ const Document = () => {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="true"
+          crossOrigin="anonymous"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&family=Varela+Round&display=swap"
