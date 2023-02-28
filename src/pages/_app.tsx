50310858cf708d385a4cd1af2ede4495a2f8bd80
Afo <obaone01@gmail.com>
2023-02-28 00:28:36 -0800

    updates baby

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,23 @@
-import { DefaultSeo } from 'next-seo'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
 
 import { Layout } from 'components/Layout'
-import { DEFAULT_SEO } from 'lib/config/seo'
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { route, asPath } = useRouter()
+  const canonicalRoute = route === '/' ? '' : `${asPath}`
+
   return (
     <>
-      <DefaultSeo {...DEFAULT_SEO} />
+      <Head>
+        <link
+          rel="canonical"
+          href={`${process.env.NEXT_PUBLIC_WEBSITE_URL}${canonicalRoute}`}
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
